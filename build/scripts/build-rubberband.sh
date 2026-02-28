f19a146f7469bdac81af90bdaba94fd6aa6a8c00
mohdsabahat <mohdsabahat123@gmail.com>
2026-02-28 14:56:12 +0530

    Fix rubberband build: use single-file compilation instead of non-existent Makefile.em

--- build/scripts/build-rubberband.sh
+++ build/scripts/build-rubberband.sh
@@ -15,6 +15,7 @@ CONF_FLAGS=(
 )
 echo "CONF_FLAGS=${CONF_FLAGS[@]}"
 
+# Build libsamplerate (has autogen.sh)
 LIB_PATH1=modules/libsamplerate
 (cd $LIB_PATH1 && \
   emconfigure ./autogen.sh && \
@@ -22,22 +23,47 @@ LIB_PATH1=modules/libsamplerate
 emmake make -C $LIB_PATH1 clean
 emmake make -C $LIB_PATH1 install
 
-LIB_PATH2=modules/libsndfile
-(cd $LIB_PATH2 && \
-  emconfigure ./autogen.sh && \
-  CFLAGS=$CFLAGS emconfigure ./configure "${CONF_FLAGS[@]}")
-emmake make -C $LIB_PATH2 clean
-emmake make -C $LIB_PATH2 install
-
+# Build rubberband using the single-file compilation approach
+# This avoids the need for meson and external dependencies
 LIB_PATH=modules/rubberband
-(CPATH="${BUILD_DIR}/include" \
-  OPTFLAGS="${OPTIM_FLAGS} -DNDEBUG -ffast-math -ftree-vectorize" \
-  emmake make -C $LIB_PATH -f otherbuilds/Makefile.em clean distclean default)
+RUBBERBAND_SRC=$LIB_PATH/single/RubberBandSingle.cpp
 
-cp $LIB_PATH/lib/librubberband.a $BUILD_DIR/lib
-cp $LIB_PATH/rubberband.pc.in $EM_PKG_CONFIG_PATH/rubberband.pc
-sed -i -e 's,%PREFIX%,'"$BUILD_DIR"',g' $EM_PKG_CONFIG_PATH/rubberband.pc
-  
+echo "Building rubberband from single-file source..."
+mkdir -p $BUILD_DIR/lib
 mkdir -p $BUILD_DIR/include/rubberband
-cp $LIB_PATH/rubberband/RubberBandStretcher.h $BUILD_DIR/include/rubberband/RubberBandStretcher.h
-cp $LIB_PATH/rubberband/rubberband-c.h $BUILD_DIR/include/rubberband/rubberband-c.h
+
+# Compile the single-file build to a static library
+# Use builtin FFT and resampler (no external dependencies)
+em++ $OPTIM_FLAGS -DNDEBUG -ffast-math -ftree-vectorize \
+  -DUSE_BQRESAMPLER=1 \
+  -DNO_TIMING=1 \
+  -DNO_THREADING=1 \
+  -DNO_THREAD_CHECKS=1 \
+  -DUSE_BUILTIN_FFT=1 \
+  -I$LIB_PATH \
+  -c $RUBBERBAND_SRC \
+  -o $BUILD_DIR/lib/rubberband.o
+
+# Create static library
+emar rcs $BUILD_DIR/lib/librubberband.a $BUILD_DIR/lib/rubberband.o
+rm $BUILD_DIR/lib/rubberband.o
+
+# Copy headers
+cp $LIB_PATH/rubberband/RubberBandStretcher.h $BUILD_DIR/include/rubberband/
+cp $LIB_PATH/rubberband/rubberband-c.h $BUILD_DIR/include/rubberband/
+
+# Create pkg-config file
+cat > $EM_PKG_CONFIG_PATH/rubberband.pc << EOF
+prefix=$BUILD_DIR
+exec_prefix=\${prefix}
+libdir=\${exec_prefix}/lib
+includedir=\${prefix}/include
+
+Name: rubberband
+Description: Rubber Band audio time-stretching and pitch-shifting library
+Version: 4.0.0
+Libs: -L\${libdir} -lrubberband
+Cflags: -I\${includedir}
+EOF
+
+echo "Rubberband build complete."
